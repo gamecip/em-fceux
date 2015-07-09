0ebfcc83dfdd2afd9e69936c0642fe5af3e09677
tsone <rnd@nic.fi>
2015-07-09 16:02:00 +0800

    0.2.3-alpha release

--- src/version.h
+++ src/version.h
@@ -42,7 +42,7 @@
 #ifndef EMSCRIPTEN
 #define FCEU_FEATURE_STRING ""
 #else
-#define FCEU_FEATURE_STRING " (em-fceux 0.2.2-alpha) Powered by Emscripten"
+#define FCEU_FEATURE_STRING " (em-fceux 0.2.3-alpha) Powered by Emscripten"
 #endif
 
 #ifdef _DEBUG
